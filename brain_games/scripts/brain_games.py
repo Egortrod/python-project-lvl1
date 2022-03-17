2ca8c7f942f3c118ec88c44b7fdc8eecacd06cd5
Egortrod <egor.arkhipoff@yandex.ru>
2022-03-17 15:18:17 +0300

    3d step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+import sys
+sys.path.append('/home/egortrod/python-project-lvl1/brain_games')
+from cli import welcome_user
+
+# from brain_games.cli import welcome_user
 
 def main():
     print ('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
