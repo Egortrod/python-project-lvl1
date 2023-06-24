b5db4f5decfa94f292b837ee7daaae3e5ae25edb
egortrod <egor.arkhipoff@yandex.ru>
2023-06-24 17:50:37 +0300

    step3 / add cli.py / change brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
+def welcome():
+    print('Welcome to the Brain Games!')
+
 
 def main():
-    print('Welcome to the Brain Games!')
+    welcome()
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
