bb8e7c63c53c074da1919f86860654a846499095
egortrod <egor.arkhipoff@yandex.ru>
2023-06-26 17:29:21 +0300

    step5 / add brain_even.py / change README.md and Makefile

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Egortrod/python-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Egortrod/python-project-lvl1/actions)
 
 <a href="https://codeclimate.com/github/Egortrod/project1/maintainability"><img src="https://api.codeclimate.com/v1/badges/8877bd0d9309c6fabbfb/maintainability" /></a>
+
+https://asciinema.org/a/p0cfV9B3n3xvsXY9QP8A39gzV
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+from random import randint
+# from brain_games.cli import welcome_user
+import prompt
+
+
+def welcome():
+    print('Welcome to the Brain Games!')
+
+
+def welcome_user():
+    global name
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
+
+def welcome_even_or_not():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+def even_or_not_game():
+    correct_answers = 0
+    while correct_answers != 3:
+        question = randint(1, 100)
+        if question % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        print(f'Question: {question}')
+        user_input = input('Your answer: ')
+        if user_input == 'yes' and question % 2 == 0:
+            print('Correct!')
+            correct_answers += 1
+        elif user_input == 'no' and question % 2 == 1:
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(f"'{user_input}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'."
+                  f"\nLet's try again, {name}!")
+            return
+    if correct_answers == 3:
+        print(f'Congratulations, {name}!')
+
+
+def main():
+    welcome()
+    welcome_user()
+    welcome_even_or_not()
+    even_or_not_game()
+
+
+if __name__ == '__main__':
+    main()
